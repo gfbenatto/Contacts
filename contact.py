5268233f1e13c93cfb76cdd5adc3f71365fb706f
torlax <torlax@gmail.com>
2019-04-24 12:43:11 +0100

    Simple code for contact register

---
+++ contact.py
@@ -0,0 +1,23 @@
+def create_contact(*args):
+    print("Enter contact information")
+    info = {}
+    for arg in args:
+        info[arg] = input('Type it %s: ' % arg)
+    return info
+
+def save_contact(**kwargs):
+    arquivo = open('contacts.csv', 'a')
+    arquivo.write(','.join(kwargs.values()))
+    arquivo.write('\n')
+    arquivo.flush()
+    arquivo.close()
+
+'banana,maça,morango'.split(',')
+
+amount = input("How many contacts do you want to register? ")
+amount = int(amount)
+field = input("Enter the fields separated by commas: ")
+field = field.split(',')
+for i in range(amount):
+    contact = create_contact(*field)
+    save_contact(**contact)
